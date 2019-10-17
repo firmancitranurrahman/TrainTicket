5d28dc64111bca0aed60414bc4236e300312616b
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-10-17 10:35:37 +0700

    Menyelesaikan Tampilan Login

--- app/src/main/java/com/example/trainticket/HomeActivity.java
+++ app/src/main/java/com/example/trainticket/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.trainticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
